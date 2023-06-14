f1c2a8a4957457cf958280f6ddbf527eb23f0fa2
AlexSa1000 <56562013+AlexSa1000@users.noreply.github.com>
2023-06-14 01:16:44 +0100

    Added interface FeatureFlagDisplayItemGenerator

---
+++ src/main/java/com/alex/cucumber/util/FeatureFlagDisplayItemGenerator.java
@@ -0,0 +1,39 @@
+package com.alex.cucumber.util;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
+
+import java.util.Arrays;
+import java.util.function.Supplier;
+
+@FunctionalInterface
+public interface FeatureFlagDisplayItemGenerator {
+    void accept(CreativeModeTab.ItemDisplayParameters parameters, Output output);
+
+    static CreativeModeTab.DisplayItemsGenerator create(FeatureFlagDisplayItemGenerator generator) {
+        return (parameters, output) -> generator.accept(parameters, Output.from(output));
+    }
+
+    interface Output extends CreativeModeTab.Output {
+        static Output from(CreativeModeTab.Output output) {
+            return output::accept;
+        }
+
+        default void accept(ItemStack stack, FeatureFlag... flags) {
+            if (Arrays.stream(flags).allMatch(FeatureFlag::isEnabled)) {
+                this.accept(stack);
+            }
+        }
+
+        default void accept(Supplier<? extends ItemLike> item) {
+            this.accept(new ItemStack(item.get()));
+        }
+
+        default void accept(Supplier<? extends ItemLike> item, FeatureFlag... flags) {
+            if (Arrays.stream(flags).allMatch(FeatureFlag::isEnabled)) {
+                this.accept(new ItemStack(item.get()));
+            }
+        }
+    }
+}
